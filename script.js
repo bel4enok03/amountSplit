be851a2e72401e673173d000b1a04890febd78c4
bel4enok03 <belskaya-tanyush@mail.ru>
2022-10-07 16:58:40 +0400

    amountSplit

---
+++ script.js
@@ -0,0 +1,29 @@
+const button = document.querySelector("#btn");
+button.addEventListener("click", calculateAmount);
+
+
+function calculateAmount(e) {
+    e.preventDefault(); 
+    const amount = document.querySelector("#amount").value;
+    const people = document.querySelector("#people").value;
+
+    console.log(amount);
+
+
+    if (amount === "" || people === "" || people < 1) {
+        Swal.fire({
+        icon: 'error',
+        title: 'Error!',
+        text: 'Please enter your information!'
+    })
+    return false;
+    }
+
+
+    let amountPerPerson = amount / people;
+
+    amountPerPerson = amountPerPerson.toFixed(2);
+
+    document.querySelector("#dividedBill").textContent = amountPerPerson;
+
+}
